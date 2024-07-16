dfae87d09624c3527730508caef1682db9969736
kimwoolina <rladnfls0500@gmail.com>
2024-07-16 20:32:19 +0900

    팩토리얼 - 재귀함수

---
+++ factorial.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+def factorial(i):
+    if i == 0:
+        return 1
+    
+    result = i * factorial(i-1)
+    return result
+
+def solution(n):
+    for i in range(1, 3628800):
+        if factorial(i) < n:
+            pass
+        elif factorial(i) > n:
+            break
+    
+    return i-1
+            
+
+print(solution(3628800))
