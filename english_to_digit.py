0e40f38ad88c2674adeb658ecc332e73f30496b5
kimwoolina <rladnfls0500@gmail.com>
2024-07-25 19:46:24 +0900

    숫자 문자열과 영단어

---
+++ english_to_digit.py
@@ -0,0 +1,29 @@
+english_dict = {"zero": "0",
+                "one": "1",
+                "two": "2",
+                "three": "3",
+                "four": "4",
+                "five": "5",
+                "six": "6",
+                "seven": "7",
+                "eight": "8",
+                "nine": "9"}
+
+def solution(s):
+    for num in english_dict:
+        s = s.replace(num, english_dict[num])
+        
+    return int(s)
+
+
+# def solution(s):
+#     words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+#     for i in range(len(words)):
+#         s = s.replace(words[i], str(i))
+#     return int(s)
+
+#test
+print(solution("one4seveneight")) #1478
+print(solution("23four5six7")) #234567
+print(solution("2three45sixseven")) #234567
+print(solution("123")) #23
