46977cb5d03ad452fff368cdcbf688c1211af7be
kimwoolina <rladnfls0500@gmail.com>
2024-07-30 18:36:22 +0900

    000의 케이스 해결

--- max_number_combination.py
+++ max_number_combination.py
@@ -8,8 +8,16 @@ def custom_compare(n1, n2):
     return int(num1 + num2) - int(num2 + num1)
 
 def largest_number(numbers):
+    # 숫자들을 정렬
     sorted_numbers = sorted(numbers, key=functools.cmp_to_key(custom_compare), reverse=True)
+    
+    # 결과 조합
     largest = "".join(map(str, sorted_numbers))
+    
+    # 모든 숫자가 0인 경우 처리
+    if largest[0] == '0':
+        return '0'
+    
     return largest
 
 def solution(numbers):
@@ -17,5 +25,6 @@ def solution(numbers):
 
 
 # test
+print(solution([0, 0 ,0, 0 ,0 ,0])) # "0"
 print(solution([6, 10, 2]))	# "6210"
 print(solution([3, 30, 34, 5, 9]))	# "9534330"
